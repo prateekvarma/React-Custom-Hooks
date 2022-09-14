e7d3e9bcd7af212e81545772b6dbc0d45b9fa650
Prateek Varma <prtk.varma@gmail.com>
2022-09-14 12:51:53 -0600

    Configured the custom hook to recieve a parameter, now it can compare and return different values

--- src/hooks/use-counter.js
+++ src/hooks/use-counter.js
@@ -1,15 +1,19 @@
 import { useState, useEffect } from "react";
 
-const useCounter = () => {
+const useCounter = (countForward) => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
+      if (countForward) {
+        setCounter((prevCounter) => prevCounter + 1);
+      } else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [countForward]);
 
   return counter; //As exception that applied to custom hooks - you can retuen values
 };
